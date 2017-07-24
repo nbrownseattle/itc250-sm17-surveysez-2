65441664a843526650b608846d2e82129d90818b
nbrownseattle <nbrown28@seattlecentral.edu>
2017-07-23 22:34:12 -0700

    Adds Survey class
    Our first version of the Survey class

--- survey_view.php
+++ survey_view.php
@@ -29,43 +29,23 @@ if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystri
 	myRedirect(VIRTUAL_PATH . "surveys/index.php");
 }
 
-//sql statement to select individual item
-/*$sql = "select MuffinName,Description,MetaDescription,MetaKeywords,Price from test_Muffins where MuffinID = " . $myID;*/
-$sql = "select Title,Description from sm17_surveys where SurveyID = " . $myID;
-//---end config area --------------------------------------------------
+$mySurvey = new Survey($myID);
+#dumpDie($mySurvey); #var dump with less writing
 
-$foundRecord = FALSE; # Will change to true, if record found!
-   
-# connection comes first in mysqli (improved) function
-#trigger is handling db connection errors. gets the error and displays it.
-$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
-
-if(mysqli_num_rows($result) > 0)
-{#records exist - process
-	   $foundRecord = TRUE;	
-	   while ($row = mysqli_fetch_assoc($result))
-	   {
-			$Title = dbOut($row['Title']);
-			$Description = dbOut($row['Description']);	
-	   }
-}
-
-@mysqli_free_result($result); # We're done with the data!
-
-if($foundRecord)
+if($mySurvey->IsValid)
 {#only load data if record found
-	$config->titleTag = $Title . " surveys made with PHP & love!"; #overwrite PageTitle with Muffin info!	
+	$config->titleTag = $mySurvey->Title . " surveys made with PHP & love!"; #overwrite PageTitle with Muffin info!	
 }
 
 # END CONFIG AREA ---------------------------------------------------------- 
 
 get_header(); #defaults to theme header or header_inc.php   
 
-if($foundRecord)
+if($mySurvey->IsValid)
 {#records exist - show survey!
 echo '
-<h3 align="center">' . $Title . '</h3>
-    <p>' . $Description . '</p>
+<h3 align="center">' . $mySurvey->Title . '</h3>
+    <p>' . $mySurvey->Description . '</p>
 ';
 		
 
@@ -75,4 +55,37 @@ echo '
 echo '<div align="center"><a href="' . VIRTUAL_PATH . 'surveys/index.php">Back</a></div>';
 
 get_footer(); #defaults to theme footer or footer_inc.php
-?>
+
+#creates a class
+class survey{
+    #add properties to the class and we set a default value for the SurveyID
+    public $SurveyID = 0;
+    public $Title = ''; 
+    public $Description = ''; 
+    public $IsValid = false;
+    #constructor to access the SurveyID from database
+    public function __construct($id)
+    {
+        
+        $id = (int)$id; #cast to integer dissalows SQL injection
+        $sql = "select Title,Description from sm17_surveys where SurveyID = " . $id;
+        
+        
+        #trigger is handling db connection errors. gets the error and displays it.
+        $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+        
+
+        if(mysqli_num_rows($result) > 0)
+        {#records exist - process 
+            $this->IsValid = true; #record found!
+	        while ($row = mysqli_fetch_assoc($result))
+	        {
+			$this->Title = dbOut($row['Title']);
+			$this->Description = dbOut($row['Description']);	
+            }
+        }
+
+        @mysqli_free_result($result); # We're done with the data!
+              
+    }//end survey constructor
+}//end survey class
